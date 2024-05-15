8ce4109a44bbbfa79abdef0e7bbc29587501d090
JacobTumak <jacobtumak@gmail.com>
2024-05-14 18:39:48 -0700

    Demo app improvements
    - Fixed bugs with assignments, added status info to detail and list view
    - improve & update auto db population for assignments app
    article.total_likes is now integer field
    increment/decrement total_likes when article is liked or a like is revoked
    bug fix
    General Formatting
    stopped tracking migrations in demo app
    remove TODO
    increment/decrement total_likes in like_article_view

--- demo/assignments/approvals.py
+++ demo/assignments/approvals.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.models import User
+from django.utils.functional import SimpleLazyObject
+
 from signoffs.approvals import ApprovalSignoff, SimpleApproval
-from signoffs.models import ApprovalSignet
 from signoffs.registry import register
 from signoffs.signing_order import SigningOrder
 
@@ -10,26 +12,22 @@ class NewAssignmentApproval(SimpleApproval):
     label = "Signoff for New Assignment"
 
     assign_project_signoff = S.register(
-        signetModel=ApprovalSignet,
         id="assign_project_signoff",
         label="Assign Project",
         perm="is_staff",
     )
 
     accept_project_signoff = S.register(
-        signetModel=ApprovalSignet,
         id="accept_project_signoff",
-        label="Accept Assignment",  # Add perm to check that the user is the assignee?
+        label="Accept Assignment",
     )
 
     submit_completed_signoff = S.register(
-        signetModel=ApprovalSignet,
         id="submit_completed_signoff",
-        label="Submit Completed",  # Add perm to check that the user is the assignee?
+        label="Submit Completed",
     )
 
     confirm_completion_signoff = S.register(
-        signetModel=ApprovalSignet,
         id="confirm_completion_signoff",
         label="Confirm Completion",
         perms="is_staff",
@@ -41,3 +39,16 @@ class NewAssignmentApproval(SimpleApproval):
         submit_completed_signoff,  # submitted
         confirm_completion_signoff,  # completed - unrevokable?
     )
+
+    def next_signoffs(self, for_user=None):
+        if not for_user:
+            return super().next_signoffs()
+        if not type(for_user) in (User, SimpleLazyObject):
+            raise TypeError(f"var \"for_user\" must be User instance, instead got {type(for_user)}\n")
+
+        assignment = self.subject
+        if (for_user == assignment.assigned_by and assignment.status in ['draft', 'pending_review']) or (for_user == assignment.assigned_to and assignment.status in ['requested', 'in_progress']):
+            return super().next_signoffs(for_user=for_user)
+        else:
+            return []
+
