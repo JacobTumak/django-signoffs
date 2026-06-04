9996d7937dc6840affb1d37f1f359093c42d9727
powderflask <powderflask@gmail.com>
2026-06-04 10:30:54 -0700

    Bump version: 0.3.12 --> 0.4.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,9 +17,9 @@ copyright = "2022, Joseph Fall"
 author = "Joseph Fall"
 
 # The short X.Y version.
-version = "0.3.12"
+version = "0.4.0"
 # The full version, including alpha/beta/rc tags.
-release = "0.3.12"
+release = "0.4.0"
 
 # The master toctree document.
 master_doc = "home"
